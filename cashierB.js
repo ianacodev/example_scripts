8d4903aa30648f04b8b57d8e688337e0c2041a6c
ianaco70 <josh.ianacone@marlettefunding.com>
2018-04-02 17:35:03 -0400

    add example web workers

---
+++ cashierB.js
@@ -0,0 +1,9 @@
+'use strict';
+this.addEventListener('message', (event) => {
+    const customers = event.data;
+    customers.forEach((customer) => { 
+        customer.status = true
+        console.log(`Cashier B : Customer ${customer.id} ${customer.status}`); 
+    });
+    this.postMessage({cashierId: 'B', customers: customers});
+});
