54821e9fdea00c696140e1d7179c7bed210a2ac8
ianaco70 <josh.ianacone@marlettefunding.com>
2018-04-03 00:08:13 -0400

    update files

--- cashierA.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-this.addEventListener('message', (event) => {
-    const customers = event.data;
-    customers.forEach((customer) => { 
-        customer.status = true
-        console.log(`Cashier A : Customer ${customer.id} ${customer.status}`); 
-    });
-    this.postMessage({cashierId: 'A', customers: customers});
-});
--- cashierB.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-this.addEventListener('message', (event) => {
-    const customers = event.data;
-    customers.forEach((customer) => { 
-        customer.status = true
-        console.log(`Cashier B : Customer ${customer.id} ${customer.status}`); 
-    });
-    this.postMessage({cashierId: 'B', customers: customers});
-});
